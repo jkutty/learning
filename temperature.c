800ae1a61f87dbdcf8bdbdaada491d3b69c9a41c
Gareth Morgan <goc.soc@gmail.com>
2026-04-18 06:40:04 -0700

    reverse the order of temps

--- temperature.c
+++ temperature.c
@@ -6,6 +6,6 @@ int main()
     int fahr;
 
     printf("  F \t C\n");
-    for (fahr = 0; fahr <= 300; fahr = fahr +20)
+    for (fahr = 300; fahr >= 0; fahr = fahr -20)
         printf("%3d |%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
