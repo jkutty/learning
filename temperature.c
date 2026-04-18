4e91a46f884807a2f4eeaf1ad3fbc3e86fcde770
Gareth Morgan <goc.soc@gmail.com>
2026-04-17 21:18:41 -0700

    tweaks to temperature

--- temperature.c
+++ temperature.c
@@ -5,6 +5,7 @@ int main()
 {
     int fahr;
 
+    printf("  F \t C\n");
     for (fahr = 0; fahr <= 300; fahr = fahr +20)
-        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        printf("%3d |%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
