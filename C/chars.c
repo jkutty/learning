901a03b555f513d25adb69af1e540ec08c52a385
Gareth Morgan <goc.soc@gmail.com>
2026-05-10 07:14:49 -0700

    end of 1.5.2

--- C/chars.c
+++ C/chars.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input; 2nd version */
 int main(void)
 {
-    long nc;
-
+    double nc;
+/*
     nc = 0;
     while (getchar() != EOF)
         ++nc;
     printf("%ld\n", nc);
+*/
+
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
